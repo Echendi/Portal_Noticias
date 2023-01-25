f067ea06918154e317c29f434ee368e318a1121a
David Fajardo <79338035+Echendi@users.noreply.github.com>
2023-01-25 17:40:59 -0500

    Instalación de dependencias necesarias

--- BE_Portal_Noticias/src/index.js
+++ BE_Portal_Noticias/src/index.js
@@ -1,1 +1,48 @@
-console.log("Hello Portal_Noticias");
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const passport = require('passport');
+const express_session = require('express-session');
+
+// Constantes
+const PORT=  5000;
+
+// Initializaciones
+const app = express();
+require('./lib/passport');
+
+// Configuraciones
+app.set('port', process.env.PORT || PORT);
+
+// middlewares
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express_session({ secret: 'SECRET' })); // Sesión secreta
+app.use(passport.initialize());
+app.use(passport.session()); // Inicio de sesión Persistente
+
+// Variables Globales 
+app.use((req, res, next) => {
+    app.locals.user = req.user;
+    next();
+});
+
+// Rutas
+app.use(require('./routes/authentication'));
+
+// Publico
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Iniciando servidor
+app.listen(app.get('port'), () => {
+    console.log('Server on port ' + app.get('port'));
+});
